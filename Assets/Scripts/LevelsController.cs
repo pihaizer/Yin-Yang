7eb5412c3bf9a5b9b01f479ded0c9b81754f454b
Vladimir Malhin <malhin.vladimir@gmail.com>
2021-02-22 13:54:40 +0300

    add simple tutorial and saves

--- Assets/Scripts/LevelsController.cs
+++ Assets/Scripts/LevelsController.cs
@@ -3,13 +3,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class LevelsController : MonoBehaviour {
     public int loadLevelNumber = 1;
     public CanvasGroup gamePassedScreen;
     public List<Map> levels;
-    public static int staticLoadLevelNumber = 0;
+    //public static int staticLoadLevelNumber = 0;
     Map _loadedLevel;
     private void Awake() {
         foreach (var level in levels) {
@@ -17,15 +18,23 @@ public class LevelsController : MonoBehaviour {
             level.Passed += OnLevelPassed;
             level.Failed += OnLevelFailed;
         }
-        if (staticLoadLevelNumber > 0) {
-            loadLevelNumber = staticLoadLevelNumber;
-            staticLoadLevelNumber = 0;
+        if (PlayerPrefs.HasKey("level")) {
+            loadLevelNumber = PlayerPrefs.GetInt("level");
         }
+        //if (staticLoadLevelNumber > 0) {
+        //    loadLevelNumber = staticLoadLevelNumber;
+        //    staticLoadLevelNumber = 0;
+        //} else 
         StartLevel(loadLevelNumber);
+    }
+    private void Update() {
+        var keyboard = Keyboard.current;
+        if (keyboard.escapeKey.IsActuated()) Application.Quit();
     }
     void StartLevel(int number) {
         if (number <= 0 || number > levels.Count) return;
         _loadedLevel = levels[number - 1];
+        PlayerPrefs.SetInt("level", number);
         _loadedLevel.gameObject.SetActive(true);
         _loadedLevel.Init();
     }
@@ -35,6 +44,7 @@ public class LevelsController : MonoBehaviour {
     async void OnLevelPassed() {
         if(levels.IndexOf(_loadedLevel) + 2 > levels.Count) {
             gamePassedScreen.DOFade(1, 1f);
+            PlayerPrefs.SetInt("level", 1);
             return;
         }
         await LoadingScreen.I.Open();
@@ -43,7 +53,7 @@ public class LevelsController : MonoBehaviour {
         _ = LoadingScreen.I.Close();
     }
     void OnLevelFailed() {
-        staticLoadLevelNumber = levels.IndexOf(_loadedLevel) + 1;
+        //staticLoadLevelNumber = PlayerPrefs.GetInt("level");// levels.IndexOf(_loadedLevel) + 1;
         LoadingScreen.I.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
